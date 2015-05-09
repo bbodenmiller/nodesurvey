3cdabb67ad58727c1458baba6407d4a30c835727
Philip <philipthov@gmail.com>
2015-05-09 15:36:47 -0700

    Init..

---
+++ survey_data.js
@@ -0,0 +1,15 @@
+// Hard coded survey questions
+module.exports = [
+    {
+        text: 'Please tell us your age.',
+        type: 'number'
+    },
+    {
+        text: 'Have you ever been to a hackathon?',
+        type: 'boolean'
+    },
+    {
+        text: 'What is your favorite car?',
+        type: 'text'
+    }
+];
