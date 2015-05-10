9d92638ab97662e049053ef74449b9a260473595
Kirby Shabaga <kirby.shabaga@gmail.com>
2015-05-10 09:01:56 -0700

    better votes collection handling on startup

--- index.js
+++ index.js
@@ -12,41 +12,62 @@ var results = require('./routes/results');
 // initialize MongoDB connection
 mongoose.connect(config.mongoUrl);
 
-// TODO: popualte the Votes collection first!
-var teamNames = [
-	"Alpha",
-	"Bravo",
-	"Charlie",
-	"Delta",
-	"Echo",
-	"Foxtrot",
-	"Golf",
-	"Hotel",
-	"India",
-	"Juliet"
-];
-
 var Votes = require('./models/Votes');
-Votes.remove({}, function(err) { 
-   console.log('votes collection removed') 
-});
+var teamNames = [
+  "Alpha",
+  "Bravo",
+  "Charlie",
+  "Delta",
+  "Echo",
+  "Foxtrot",
+  "Golf",
+  "Hotel",
+  "India",
+  "Juliet"
+];
+mongoose.connection.on('open', function (ref) {
+    var init = true;
+    console.log('Connected to mongo server.');
+    //trying to get collection names
+    mongoose.connection.db.collectionNames(function (err, names) {
+        console.log(names); // [{ name: 'dbname.myCollection' }]
+        for (var i in names) {
+          if (names[i].name === 'n3kidvote.votes') {
+            init = false;
+            console.log('The votes collection already exists');
+          }
+        }
+        if (init) {
+          initVotesCollection();
+        }
+    });
+})
 
-for (var i in teamNames) {
+function initVotesCollection() {
+  // add the teams to 'votes' collection
+  console.log('initializing the votes collection');
+  for (var i in teamNames) {
 
-	var teamNumber = parseInt(i, 10) + 1;
-	console.log('teamNumber = ' + teamNumber);
+    var teamNumber = parseInt(i, 10) + 1;
+    console.log('teamNumber = ' + teamNumber);
 
-	var votes = new Votes({
-		teamNumber: teamNumber,
-		teamName: teamNames[i],
-		votes: 0
-	});
+    var votes = new Votes({
+      teamNumber: teamNumber,
+      teamName: teamNames[i],
+      votes: 0
+    });
 
-	votes.save(function(err, newSub) {
-    	if (err || !newSub) 
-        	console.log('Could not create votes document');
-	});
+    votes.save(function(err, newSub) {
+        if (err || !newSub) 
+            console.log('Could not create votes document');
+    });
+  } 
 }
+
+// Votes.remove({}, function(err) { 
+//    console.log('votes collection removed') 
+// });
+
 
 // Create Express web app with some useful middleware
 var app = express();
